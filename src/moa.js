58b512bce4076a05b573b1a201cab414755a06eb
Sergei Danilov <pencroff@gmail.com>
2013-08-27 18:21:24 +0300

    Add Moa.js module

---
+++ src/moa.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * Project: MoaJs
+ * User: Sergii Danilov
+ * Date: 8/27/13
+ * Time: 6:02 PM
+ */
+/*global define:true*/
+define('Moa', ['obj'], function (obj) {
+    'use strict';
+    var _moa = {
+        Obj: obj
+    };
+    return _moa;
+});
