1c4660ec85bdb08efeb2421cbbc2827176dd3b14
Pencroff <pencroff@gmail.com>
2013-08-27 07:42:33 +0300

    Start working above 'obj' part

---
+++ src/obj.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm by Pencroff for MoaJs.
+ * Date: 27.08.2013
+ * Time: 7:32
+ */
+/*global define.true*/
+define('obj', [], function () {
+    var obj = function() {},
+        map={};
+    return obj;
+});
