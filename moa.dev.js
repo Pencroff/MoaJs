28b868d5ab87f8a109336122fcc2f596371068dd
Pencroff <pencroff@gmail.com>
2014-03-22 00:40:32 +0000

    Update current files

--- moa.dev.js
+++ moa.dev.js
@@ -7,19 +7,21 @@
             return new F();
         };
     }());
-    var undef, fn = "function", ob = "object", un = "undefined", map = {}, mixins = {}, extend = function(target, source) {
+    var undef, fn = "function", ob = "object", un = "undefined", str = "string", map = {}, mixins = {}, depthRecursion = 64, extend = function(target, source) {
         var prop;
-        for (prop in source) source.hasOwnProperty(prop) && (target[prop] = source[prop]);
-        target.toString = source.toString;
-        target.valueOf = source.valueOf;
-        target.toLocaleString = source.toLocaleString;
+        if (source) {
+            for (prop in source) source.hasOwnProperty(prop) && (target[prop] = source[prop]);
+            target.toString = source.toString;
+            target.valueOf = source.valueOf;
+            target.toLocaleString = source.toLocaleString;
+        }
         return target;
     }, throwWrongParamsErr = function(method, param) {
         var msg = "Wrong parameters in " + method;
         param && (msg = "Wrong parameter " + param + " in " + method);
         throw new Error(msg, "Moa");
     }, throwWrongType = function(obj, extendType, isMixin) {
-        var undef, type = "Type ";
+        var type = "Type ";
         if (obj === undef) {
             isMixin === !0 && (type = "Mixin type ");
             throw new Error(type + extendType + " not found", "Moa");
@@ -78,6 +80,40 @@
             $ctor: $ctor,
             $base: $base
         };
+    }, resolveDeclaration = function(type, diConfiguration) {
+        var configurationProperty, configurationValue, typeObj;
+        diConfiguration = diConfiguration || {};
+        diConfiguration.$current || (diConfiguration.$current = type);
+        for (configurationProperty in diConfiguration) {
+            if ("$ctor" === configurationProperty) {
+                configurationValue = resolveDeclaration(type, diConfiguration[configurationProperty]);
+                delete configurationValue.$current;
+            } else {
+                configurationValue = diConfiguration[configurationProperty];
+                switch (typeof configurationValue) {
+                  case str:
+                    if ("$current" === configurationProperty) configurationValue = {
+                        type: configurationValue,
+                        instance: "item",
+                        lifestyle: "transient"
+                    }; else {
+                        typeObj = map[configurationValue];
+                        typeObj && (configurationValue = typeObj.$di.$current);
+                    }
+                    break;
+
+                  case ob:
+                    "$current" === configurationProperty && (configurationValue.type = type);
+                    configurationValue.type || throwWrongParamsErr("define", "$di -> type");
+                    if ("ctor" === configurationValue.instance) delete configurationValue.lifestyle; else {
+                        configurationValue.instance = "item";
+                        configurationValue.lifestyle && "singleton" !== configurationValue.lifestyle && (configurationValue.lifestyle = "transient");
+                    }
+                }
+            }
+            diConfiguration[configurationProperty] = configurationValue;
+        }
+        return diConfiguration;
     }, Moa = {
         define: function(type, definition) {
             var mapObj, baseType, base, len = arguments.length;
@@ -115,6 +151,7 @@
                     throwWrongParamsErr("define", "definition");
                 }
                 map[type] = mapObj;
+                map[type].$di = resolveDeclaration(type, mapObj.$di);
                 break;
 
               default:
@@ -128,14 +165,72 @@
                 mixins[mixType] = definition;
             } else delete mixins[mixType];
         },
-        resolve: function(type) {
-            var prop, result, Ctor = Moa.define(type), info = Moa.getTypeInfo(type), di = info.$di;
-            result = new Ctor();
-            for (prop in di) {
-                Ctor = Moa.define(di[prop]);
-                result[prop] = new Ctor();
-            }
-            return result;
+        resolve: function(type, configObj) {
+            var item, cnt = 0, mapObj = map[type], len = arguments.length, createItem = function(cParams, cObj, mObj) {
+                var item;
+                if (cParams) {
+                    cParams = extend(cParams, cObj);
+                    item = new mObj.$ctor(cParams);
+                } else item = new mObj.$ctor(cObj);
+                return item;
+            }, fnResolveObjConf = function(declaration, fnResolveListConf, mp, ctorParams) {
+                var ctorObj, propObj, mpObj, current = declaration.$current, ctor = declaration.$ctor, prop = declaration;
+                cnt += 1;
+                if (cnt > depthRecursion) throw new Error("Loop of recursion", "moa");
+                current || (current = declaration);
+                mpObj = mp[current.type];
+                ctor || (ctor = mpObj.$di.$ctor);
+                prop === current && (prop = mpObj.$di);
+                switch (current.instance) {
+                  case "item":
+                    ctor && (ctorObj = fnResolveListConf(ctor, fnResolveObjConf, mp));
+                    propObj = fnResolveListConf(prop, fnResolveObjConf, mp);
+                    switch (current.lifestyle) {
+                      case "transient":
+                        item = createItem(ctorParams, ctorObj, mpObj);
+                        break;
+
+                      case "singleton":
+                        current.item || (current.item = createItem(ctorParams, ctorObj, mpObj));
+                        item = current.item;
+                        break;
+
+                      default:
+                        throwWrongParamsErr("resolve", type + "::$di::$current::lifestyle");
+                    }
+                    break;
+
+                  case "ctor":
+                    item = mpObj.$ctor;
+                    break;
+
+                  default:
+                    throwWrongParamsErr("resolve", type + "::$di::$current::instance");
+                }
+                propObj && (item = extend(item, propObj));
+                return item;
+            }, fnResolveListConf = function(objDeclaration, fnResolveObjConf, mp) {
+                var prop, propValue, mpObj, result = {};
+                for (prop in objDeclaration) if ("$current" !== prop && "$ctor" !== prop) {
+                    propValue = objDeclaration[prop];
+                    if ("object" == typeof propValue) {
+                        mpObj = mp[propValue.type];
+                        result[prop] = mpObj ? fnResolveObjConf(propValue, fnResolveListConf, mp) : objDeclaration[prop];
+                    } else result[prop] = objDeclaration[prop];
+                }
+                return result;
+            };
+            1 !== len && 2 !== len && throwWrongParamsErr("resolve");
+            item = mapObj ? fnResolveObjConf(mapObj.$di, fnResolveListConf, map, configObj) : type;
+            return item;
+        },
+        clear: function() {
+            var clearObj = function(obj) {
+                var prop;
+                for (prop in obj) delete obj[prop];
+            };
+            clearObj(map);
+            clearObj(mixins);
         },
         getRegistry: function() {
             var iterate = function(obj) {
@@ -154,6 +249,8 @@
             result = extend({}, mapObj);
             delete result.$ctor;
             delete result.$base;
+            delete result.$di;
+            result.$di = JSON.parse(JSON.stringify(mapObj.$di));
             return result;
         }
     };
